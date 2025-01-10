b68de146b31bcc19e96138c15f7f0e3a7292ea5c
Liliane Lukong <liliane.haniel@gmail.com>
2025-01-10 17:43:27 +0100

    jewellery shop html, css, script files edited for mobile responsiveness

--- simple-web-pages/jewellery-shop/script.js
+++ simple-web-pages/jewellery-shop/script.js
@@ -1,34 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const hamburgerMenu = document.querySelector('.hamburger-menu');
     const navLinks = document.querySelector('.nav-links');
+    const closeBtn = document.querySelector('.close-btn');
 
-    hamburgerMenu.addEventListener('click', () => {
+    function toggleMenu() {
+        hamburgerMenu.classList.toggle('active');
         navLinks.classList.toggle('nav-active');
-        const expanded = hamburgerMenu.getAttribute('aria-expanded') === 'true' || false;
-        hamburgerMenu.setAttribute('aria-expanded', String(!expanded));
-        adjustCarouselContainer(); // Adjust carousel container size after toggling menu
-    });
+        const expanded = hamburgerMenu.getAttribute('aria-expanded') === 'true';
+        hamburgerMenu.setAttribute('aria-expanded', !expanded);
+    }
 
-    // Add event listener to submenu items
-    const subMenuLinks = document.querySelectorAll('.nav-links ul li ul li a');
-    subMenuLinks.forEach(link => {
-        link.addEventListener('click', () => {
-            adjustCarouselContainer(); // Adjust carousel container size after submenu item click
-        });
-    });
+    // Open/Close menu on hamburger click
+    hamburgerMenu.addEventListener('click', toggleMenu);
 
-    // Add event listener to main menu items
-    const mainMenuLinks = document.querySelectorAll('.nav-links > li > a');
-    mainMenuLinks.forEach(link => {
-        link.addEventListener('click', () => {
-            adjustCarouselContainer(); // Adjust carousel container size after main menu item click
-        });
-    });
+    // Close menu on close button click
+    closeBtn.addEventListener('click', toggleMenu);
 });
 
-// Function to adjust carousel container size based on header height
-function adjustCarouselContainer() {
-    const headerHeight = document.querySelector('header').offsetHeight;
-    const carouselContainer = document.querySelector('.carousel-container');
-    carouselContainer.style.marginTop = headerHeight + 'px';
-}
